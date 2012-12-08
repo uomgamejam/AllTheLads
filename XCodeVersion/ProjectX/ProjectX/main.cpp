189577d087d2c69508f2afecd4a7f401cbd4ab62
Rui Teixeira <rui.clara.teixeira@gmail.com>
2012-12-08 17:51:46 +0000

    Xcode Files

---
+++ XCodeVersion/ProjectX/ProjectX/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ProjectX
+//
+//  Created by Rui Teixeira on 08/12/12.
+//  Copyright (c) 2012 Rui Teixeira. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
