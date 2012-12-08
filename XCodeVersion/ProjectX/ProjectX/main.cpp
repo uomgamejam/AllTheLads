36359d933a0105e14bc7c3964b6bdd42c0c11134
James Barry <james.barry93@googlemail.com>
2012-12-08 23:07:41 +0000

    added graphics

--- XCodeVersion/ProjectX/ProjectX/main.cpp
+++ XCodeVersion/ProjectX/ProjectX/main.cpp
@@ -1,18 +1,62 @@
-//
-//  main.cpp
-//  ProjectX
-//
-//  Created by Rui Teixeira on 08/12/12.
-//  Copyright (c) 2012 Rui Teixeira. All rights reserved.
-//
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include "ResourcePath.hpp"
 
-#include <iostream>
-
-int main(int argc, const char * argv[])
+int main (int argc, const char * argv[])
 {
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    // Load a sprite to display
+    sf::Texture texture;
+    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg"))
+    	return EXIT_FAILURE;
+    sf::Sprite sprite(texture);
+
+    // Create a graphical text to display
+    sf::Font font;
+    if (!font.loadFromFile(resourcePath() + "sansation.ttf"))
+    	return EXIT_FAILURE;
+    sf::Text text("Hello SFML", font, 50);
+    text.setColor(sf::Color::Black);
+
+    // Load a music to play
+    sf::Music music;
+    if (!music.openFromFile(resourcePath() + "nice_music.ogg"))
+    	return EXIT_FAILURE;
+
+    // Play the music
+    music.play();
+        
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+    	// Process events
+    	sf::Event event;
+    	while (window.pollEvent(event))
+    	{
+    		// Close window : exit
+    		if (event.type == sf::Event::Closed)
+    			window.close();
+            
+    		// Escape pressed : exit
+    		if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+    			window.close();
+    	}
+
+    	// Clear screen
+    	window.clear();
+    	
+    	// Draw the sprite
+    	window.draw(sprite);
+    	
+    	// Draw the string
+    	window.draw(text);
+
+    	// Update the window
+    	window.display();
+    }
+
+	return EXIT_SUCCESS;
 }
-
